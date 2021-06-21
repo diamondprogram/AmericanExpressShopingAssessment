7c70a29610b25b76bee435cdbbef6197bf0dc20b
diamondprogram <47327898+diamondprogram@users.noreply.github.com>
2021-06-21 10:20:31 -0400

    Update mainShoopingApp.kt

--- mainShoopingApp.kt
+++ mainShoopingApp.kt
@@ -10,23 +10,23 @@ import app.Checkout;
             println("Welcome to Express Store");
             println("1. Checkout");
             println("2. exit");
-            var userOption = 0;
+            var userOption;
             //request the user to eneter an option
             //if user eneter a options that is not valid it will keep looping til option that is enterd is accepted;
             var userSeletedOption = false;
             val inputScanner = Scanner(System.`in`);
             while (!userSeletedOption) {
                 print("Select an Option: ");
-                userOption = inputScanner.nextInt();
+                userOption = inputScanner.next();
                 //if input entered by the user is not accepted and invaliud message is printed and is promted to enter an option again.
-                if (userOption != 1 && userOption != 2) {
+                if (userOption != "1" && userOption != "2") {
                     println("Invalid input detected!");
                 } else {
                     userSeletedOption = true;
                 }
             }
 
-            if (userOption == 1) {
+            if (userOption == "1") {
                 val checkout = Checkout();
                 println("We currently have apples and oranges in Stock.")
 
@@ -58,7 +58,7 @@ import app.Checkout;
                     exitProcess(1);//exits from the application
                 }
 
-            } else if (userOption == 2) {
+            } else if (userOption == "2") {
                 print("Have a great day.");
                 exitProcess(1);
             }
